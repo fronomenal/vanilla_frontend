8b40e9e4cf57f1483ebd4c0c2bcaef6dc97ae725
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-05-18 23:02:48 +0100

    initial project setup

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+import gulp from 'gulp';
+import dartSass from 'sass'; import gulpSass from 'gulp-sass'; const sass = gulpSass(dartSass);
+
+import prefix from 'gulp-autoprefixer';
+import minify from 'gulp-clean-css';
+import terser from 'gulp-terser';
+import imagemin from 'gulp-imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminOptipng from 'imagemin-optipng';
+import imagewebp from 'gulp-webp';
+
+
+function compilescss() {
+  return gulp.src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(prefix('last 2 versions'))
+    .pipe(minify())
+    .pipe(gulp.dest('app/www/css')) 
+};
+
+
+function jsmin(){
+    return gulp.src('src/js/*.js') 
+      .pipe(terser())
+      .pipe(gulp.dest('app/www/js')); 
+  }
+
+
+function optimizeimg() {
+  return gulp.src('src/imgs/*.{jpg,png}') 
+    .pipe(imagemin([
+        imageminMozjpeg({ quality: 75, progressive: true }),
+        imageminOptipng({ optimizationLevel: 3 }),
+    ]))
+    .pipe(gulp.dest('app/assets/imgs')) 
+};
+
+
+function webpImage() {
+  return gulp.src('app/assets/imgs/*.{jpg,png}') 
+    .pipe(imagewebp())
+    .pipe(gulp.dest('app/assets/imgs')) 
+};
+
+
+function watchTask(){
+    gulp.watch('src/scss/*.scss', compilescss); 
+    gulp.watch('src/js/*.js', jsmin); 
+    gulp.watch('src/images/*', optimizeimg); 
+    gulp.watch('dist/images/*.{jpg,png}', webpImage); 
+}
+
+
+ 
+export default gulp.series(
+  compilescss,
+  jsmin,
+  optimizeimg,
+  webpImage,
+  watchTask
+);
